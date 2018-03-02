b565ab97b39d3157b27a141b3dffe28fe19a3684
Caleb Thuku <nyagathuku@gmail.com>
2018-03-02 12:23:34 +0300

    Almost Complete

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ $("form#display").submit(function(event) {
   function answer(display){
     var arr = [];
     for(i=1; i<=display; i++){
-      // if the number is divisible by 15, Display Ping pong
+      // if the number is divisible by 15, Display Ping Pong
       if ( i % 15 === 0) {
         console.log("pingpong")
         arr.push("PING PONG");
